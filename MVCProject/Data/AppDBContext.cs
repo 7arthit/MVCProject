e21260cdfbf74b1180cf15c8d9bfb683f2cdb25c
7arthit <artsshit12@gmail.com>
2023-11-18 21:44:23 +0700

    Add project files.

---
+++ MVCProject/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject.Models;
+
+namespace MVCProject.Data
+{
+    public class AppDBContext : DbContext
+    {
+        //สร้าง คอนสครัคเตอร์
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; } //สร้างเก็บฐานข้อมูล
+    }
+}
